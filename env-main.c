b08583d0d65a606e94c31c81daf1c7c7d516de55
WillMeilahn <meilahnwa@gmail.com>
2023-08-15 21:12:28 -0500

    env main fix

--- env-main.c
+++ env-main.c
@@ -11,13 +11,13 @@
  */
 int main(int ac, char **av, char **env)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while(env[i] != NULL)
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (env[i] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	return (0);
 }
