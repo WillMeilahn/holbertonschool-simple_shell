493b6659c4c76da0c2e5c713152e3924967ffa2d
WillMeilahn <meilahnwa@gmail.com>
2023-08-15 20:54:05 -0500

    env mian fix

--- env-main.c
+++ env-main.c
@@ -3,6 +3,10 @@
 /**
  * main - prints the environment
  *
+ * ac - argument count
+ * av - an array of string pointers containing command line arguments
+ * env - the environment to be printed
+ *
  * Return: Always 0.
  */
 int main(int ac, char **av, char **env)
@@ -10,7 +14,7 @@ int main(int ac, char **av, char **env)
     unsigned int i;
 
     i = 0;
-    while (env[i] != NULL)
+    while(env[i] != NULL)
     {
         printf("%s\n", env[i]);
         i++;
